283714633ad72bfa07d01f1b3b031ab02ed2edbf
Dinh Vu <dinh.vu@trustingsocial.com>
2024-06-27 10:02:23 +0700

    TRAD-13 Implement service to get onboardDate from market_info by symbol

--- src/main/java/com/tradey/technical_analysis/domain/services/SymbolInfoService.java
+++ src/main/java/com/tradey/technical_analysis/domain/services/SymbolInfoService.java
@@ -4,13 +4,11 @@ import com.tradey.technical_analysis.domain.entity.SymbolInfoEntity;
 import com.tradey.technical_analysis.domain.repositories.SymbolInfoRepository;
 import lombok.RequiredArgsConstructor;
 
-import java.math.BigInteger;
-
 @RequiredArgsConstructor
 public class SymbolInfoService {
     private final SymbolInfoRepository symbolInfoRepository;
 
-    public BigInteger getOnboardDate(String exchangeType, String symbol) {
+    public Long getOnboardDate(String exchangeType, String symbol) {
         SymbolInfoEntity entity = symbolInfoRepository.get(exchangeType, symbol);
         return entity == null ? null : entity.getOnboardDate();
     }
--- src/main/java/com/tradey/technical_analysis/infrastructure/dto/SymbolInfoDTO.java
+++ src/main/java/com/tradey/technical_analysis/infrastructure/dto/SymbolInfoDTO.java
@@ -4,13 +4,11 @@ import com.tradey.technical_analysis.domain.entity.SymbolInfoEntity;
 
 import lombok.RequiredArgsConstructor;
 
-import java.math.BigInteger;
-
 @RequiredArgsConstructor
 public class SymbolInfoDTO {
     private final String exchange_type;
     private final String symbol;
-    private final BigInteger onboard_date;
+    private final long onboard_date;
 
     public SymbolInfoEntity toEntity() {
         return new SymbolInfoEntity(exchange_type, symbol, onboard_date);
